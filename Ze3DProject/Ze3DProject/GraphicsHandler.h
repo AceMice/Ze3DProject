4dff94ef9b3952de1e76d5909d38a5457972027f
Oscar Engborg <Engborg@live.se>
2016-02-07 12:00:05 +0100

    Added some constants to GraphicsHandler

--- Ze3DProject/Ze3DProject/GraphicsHandler.h
+++ Ze3DProject/Ze3DProject/GraphicsHandler.h
@@ -4,6 +4,12 @@
 #include "D3DHandler.h"
 #include "ShaderHandler.h"
 
+//Constants
+const bool FULL_SCREEN = false;
+const bool VSYNC_ENABLED = false;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
+
 class GraphicsHandler {
 private:
 
