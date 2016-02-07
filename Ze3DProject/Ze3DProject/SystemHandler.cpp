d724d5491c63482d70f26a789123b6184d4f8616
Tobbe <tobbe.kling@hotmail.com>
2016-02-07 12:17:58 +0100

    Added SystemHandler.cpp

---
+++ Ze3DProject/Ze3DProject/SystemHandler.cpp
@@ -0,0 +1,35 @@
+#include "SystemHandler.h"
+SystemHandler::SystemHandler() {
+	//InputObject = nullptr;
+	//GarpahicsObject = nullptrs;
+}
+
+SystemHandler::~SystemHandler() {
+
+}
+
+void SystemHandler::InitWindow(int screenWidth, int screenHeight) {
+
+}
+
+void SystemHandler::ShutdownWindow() {
+
+}
+
+
+bool SystemHandler::Initialize() {
+
+}
+
+void SystemHandler::Run() {
+
+}
+
+void SystemHandler::Shutdown() {
+
+}
+
+LRESULT CALLBACK SystemHandler::MessageHandler(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam) {
+
+}
+
