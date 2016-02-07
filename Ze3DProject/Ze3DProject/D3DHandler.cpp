8a17ea06ff3f7b98f5bd7909a568799b368ecabb
Oscar Engborg <Engborg@live.se>
2016-02-07 14:05:21 +0100

    Implemented D3Dhandler::initialize until depthstencil

--- Ze3DProject/Ze3DProject/D3DHandler.cpp
+++ Ze3DProject/Ze3DProject/D3DHandler.cpp
@@ -77,6 +77,7 @@ bool D3DHandler::Initialize(int screenWidth, int screenHeight, HWND hwnd, bool v
 		return false;
 	}
 	//Loop through display modes and find the one matching the screen width and height
+	//When found, store numerator and denominator of refresh rate
 	for (int i = 0; i < numModes; i++) {
 		if (displayModeList[i].Width == (unsigned int)screenWidth) {
 			if (displayModeList[i].Height == (unsigned int)screenHeight) {
@@ -86,5 +87,107 @@ bool D3DHandler::Initialize(int screenWidth, int screenHeight, HWND hwnd, bool v
 		}
 	}
 
+	//Get the adapter(gpu) description
+	hresult = adapter->GetDesc(&adapterDesc);
+	if (FAILED(hresult)) {
+		MessageBox(hwnd, L"adapter->GetDesc", L"Error", MB_OK);
+		return false;
+	}
+	//Store the dedicated video card memory in megabytes
+	this->videoCardMemory = (int)(adapterDesc.DedicatedVideoMemory / 1024 / 1024);
+	//Store the video card name in char array
+	error = wcstombs_s(&stringLength, this->videoCardDescription, 128, adapterDesc.Description, 128);
+	if (error != 0) {
+		MessageBox(hwnd, L"wcstombs_s", L"Error", MB_OK);
+		return false;
+	}
 
+	//Release the display mode list
+	delete[] displayModeList;
+	displayModeList = nullptr;
+	//Release the adapter output
+	adapterOutput->Release();
+	adapterOutput = nullptr;
+	//Release the adapter
+	adapter->Release();
+	adapter = nullptr;
+	//Release the factory
+	factory->Release();
+	factory = nullptr;
+
+	//Init the swap chain description
+	ZeroMemory(&swapChainDesc, sizeof(swapChainDesc));
+	//Set to a single back buffer
+	swapChainDesc.BufferCount = 1;
+	//Set the width and height of back buffer
+	swapChainDesc.BufferDesc.Width = screenWidth;
+	swapChainDesc.BufferDesc.Height = screenHeight;
+	//Set regular 32-bit suface for the back buffer
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+
+	//Set the refresh rate of back buffer
+	if (this->vsync_enabled) {
+		swapChainDesc.BufferDesc.RefreshRate.Numerator = numerator;
+		swapChainDesc.BufferDesc.RefreshRate.Denominator = denominator;
+	}
+	else {
+		swapChainDesc.BufferDesc.RefreshRate.Numerator = 0;
+		swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+	}
+
+	//Set the usage of back buffer
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	//Set the handle of window to render in
+	swapChainDesc.OutputWindow = hwnd;
+
+	//Turn multisampling off
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+
+	//Set to either fullscreen of window mode
+	if (fullscreen) {
+		swapChainDesc.Windowed = false;
+	}
+	else {
+		swapChainDesc.Windowed = true;
+	}
+
+	//Set the scan line ordering and scaling to unspecified
+	swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	//Discard the back buffer content after displaying
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	//No advanced flags
+	swapChainDesc.Flags = 0;
+
+	//Set the feature level to DirectX 11
+	featureLevel = D3D_FEATURE_LEVEL_11_0;
+
+	//Create the swap chain, Direct3D device and Direct3D device context
+	hresult = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, &featureLevel, 1, 
+		D3D11_SDK_VERSION, &swapChainDesc, &this->swapChain, &this->device, NULL, &this->deviceContext);
+	if (FAILED(hresult)) {
+		MessageBox(hwnd, L"D3D11CreateDeviceAndSwapChain", L"Error", MB_OK);
+		return false;
+	}
+
+	//Get the pointer to the back buffer
+	hresult = this->swapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&backBufferPtr);
+	if (FAILED(hresult)) {
+		MessageBox(hwnd, L"this->swapChain->GetBuffer", L"Error", MB_OK);
+		return false;
+	}
+	//Create the render target view with the back buffer pointer
+	hresult = this->device->CreateRenderTargetView(backBufferPtr, NULL, &this->renderTargetView);
+	if (FAILED(hresult)) {
+		MessageBox(hwnd, L"this->device->CreateRenderTargetView", L"Error", MB_OK);
+		return false;
+	}
+	//Release pointer to back buffer as it is no longer needed
+	backBufferPtr->Release();
+	backBufferPtr = nullptr;
+
+	//Continue with the depth stecil
+
+	return true;
 }
