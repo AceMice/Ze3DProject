9ba136da34dc07852dfd7513f45b643ba93bcf56
Oscar Engborg <Engborg@live.se>
2016-02-12 11:35:58 +0100

    Implemented the rest of shaderHandler::initializeShaders()

--- Ze3DProject/Ze3DProject/ShaderHandler.cpp
+++ Ze3DProject/Ze3DProject/ShaderHandler.cpp
@@ -50,6 +50,106 @@ bool ShaderHandler::Render(ID3D11DeviceContext* deviceContext, int indexCount, X
 
 bool ShaderHandler::InitializeShader(ID3D11Device* device, HWND hwnd, WCHAR* vsFilename, WCHAR* psFilename)
 {
+	HRESULT hresult;
+	ID3D10Blob* errorMessage;
+	ID3D10Blob* vertexShaderBuffer;
+	ID3D10Blob* pixelShaderBuffer;
+	D3D11_INPUT_ELEMENT_DESC polygonLayout[2];
+	unsigned int numElements;
+	D3D11_BUFFER_DESC matrixBufferDesc;
+
+	//init pointers to nullptr
+	errorMessage = nullptr;
+	vertexShaderBuffer = nullptr;
+	pixelShaderBuffer = nullptr;
+
+	//Compile the vertex shader code
+	hresult = D3DCompileFromFile(vsFilename, NULL, NULL, "main", "vs_5_0", D3D10_SHADER_ENABLE_STRICTNESS, 0, &vertexShaderBuffer, &errorMessage);
+	if (FAILED(hresult)) {
+		if (errorMessage) {
+			OutputShaderErrorMessage(errorMessage, hwnd, vsFilename);
+		}
+		else {
+			MessageBox(hwnd, L"D3DCompileFromFile(VS)", L"Error", MB_OK);
+		}
+		return false;
+	}
+
+	//Compile the pixel shader code
+	hresult = D3DCompileFromFile(psFilename, NULL, NULL, "main", "ps_5_0", D3D10_SHADER_ENABLE_STRICTNESS, 0, &pixelShaderBuffer, &errorMessage);
+	if (FAILED(hresult)) {
+		if (errorMessage) {
+			OutputShaderErrorMessage(errorMessage, hwnd, psFilename);
+		}
+		else {
+			MessageBox(hwnd, L"D3DCompileFromFile(PS)", L"Error", MB_OK);
+		}
+		return false;
+	}
+
+	//Create the vertex shader from buffer
+	hresult = device->CreateVertexShader(vertexShaderBuffer->GetBufferPointer(), vertexShaderBuffer->GetBufferSize(), NULL, &this->vertexShader);
+	if (FAILED(hresult)) {
+		MessageBox(hwnd, L"device->CreateVertexShader", L"Error", MB_OK);
+		return false;
+	}
+
+	//Create the pixel shader from buffer
+	hresult = device->CreatePixelShader(pixelShaderBuffer->GetBufferPointer(), pixelShaderBuffer->GetBufferSize(), NULL, &this->pixelShader);
+	if (FAILED(hresult)) {
+		MessageBox(hwnd, L"device->CreatePixelShader", L"Error", MB_OK);
+		return false;
+	}
+
+	//Fill the vertex input layout description 
+	//This needs to match the modelHandler and shader 
+	polygonLayout[0].SemanticName = "POSITION";
+	polygonLayout[0].SemanticIndex = 0;
+	polygonLayout[0].Format = DXGI_FORMAT_R32G32B32_FLOAT;
+	polygonLayout[0].InputSlot = 0;
+	polygonLayout[0].AlignedByteOffset = 0;
+	polygonLayout[0].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[0].InstanceDataStepRate = 0;
+
+	polygonLayout[1].SemanticName = "COLOR";
+	polygonLayout[1].SemanticIndex = 0;
+	polygonLayout[1].Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	polygonLayout[1].InputSlot = 0;
+	polygonLayout[1].AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT;
+	polygonLayout[1].InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+	polygonLayout[1].InstanceDataStepRate = 0;
+
+	//Get the number of elements in the layout
+	numElements = sizeof(polygonLayout) / sizeof(polygonLayout[0]);
+
+	//Create the vertex input layout.
+	hresult = device->CreateInputLayout(polygonLayout, numElements, vertexShaderBuffer->GetBufferPointer(), vertexShaderBuffer->GetBufferSize(), &this->layout);
+	if (FAILED(hresult)) {
+		MessageBox(hwnd, L"device->CreateInputLayout", L"Error", MB_OK);
+		return false;
+	}
+
+	//Release and nullptr the buffers as they are no longer needed
+	vertexShaderBuffer->Release();
+	vertexShaderBuffer = nullptr;
+	pixelShaderBuffer->Release();
+	pixelShaderBuffer = nullptr;
+
+	//Fill the description of the dynamic matrix constant buffer that is in the vertex shader
+	matrixBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+	matrixBufferDesc.ByteWidth = sizeof(MatrixBuffer);
+	matrixBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	matrixBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	matrixBufferDesc.MiscFlags = 0;
+	matrixBufferDesc.StructureByteStride = 0;
+
+	// Create the constant buffer pointer so we can access the vertex shader constant buffer from within this class.
+	hresult = device->CreateBuffer(&matrixBufferDesc, NULL, &this->matrixBuffer);
+	if (FAILED(hresult)) {
+		MessageBox(hwnd, L"device->CreateBuffer", L"Error", MB_OK);
+		return false;
+	}
+
 	return true;
 }
 
