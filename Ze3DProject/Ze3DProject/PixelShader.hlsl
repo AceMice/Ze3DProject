5806e2e8b10789cfa5fb5900eb53405aa505648b
Tobbe <tobbe.kling@hotmail.com>
2016-03-03 11:06:54 +0100

    Separated ambient and diffuse color callculations for easier enavle/disable

--- Ze3DProject/Ze3DProject/PixelShader.hlsl
+++ Ze3DProject/Ze3DProject/PixelShader.hlsl
@@ -30,14 +30,23 @@ float4 main(PixelInput input) : SV_TARGET
 		s = color;
 	}
 	
+	//Ambient color
+	float aRed = 0.2 * s.r;
+	float aGreen = 0.2 * s.g;
+	float aBlue = 0.2 * s.b;
+	float3 ambient = float3(aRed, aGreen, aBlue);
+	
+	//Calculate Diffuse color
 	float3 outVec = (float4(0,5,-6,1) - input.worldPos).xyz;	//(0,0,-6,1)Position of light i worldspace, camera is at (0,0,-5)
 	outVec = normalize(outVec);
 	float value = saturate(dot(input.normal, outVec));
 
-	//Calculate new color
-	s.r = (0.8 * s.r * value) + (0.2 * s.r);
-	s.g = (0.8 * s.g * value) + (0.2 * s.g);
-	s.b = (0.8 * s.b * value) + (0.2 * s.b);
+	float dRed = (0.8 * s.r * value);
+	float dGreen = (0.8 * s.g * value);
+	float dBlue = (0.8 * s.b * value);
+	float3 diffuse = float3(dRed, dGreen, dBlue);
 
-	return float4(s, 1.0f);
+	float3 result = ambient + diffuse;
+
+	return float4(result, 1.0f);
 }
