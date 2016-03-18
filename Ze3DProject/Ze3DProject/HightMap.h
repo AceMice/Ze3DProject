220890d0c29532bda5569e463d1c3c4e6bada3b8
Tobbe <tobbe.kling@hotmail.com>
2016-03-18 12:26:05 +0100

    Initialized HightMap.h

--- Ze3DProject/Ze3DProject/HightMap.h
+++ Ze3DProject/Ze3DProject/HightMap.h
@@ -1,1 +1,22 @@
-#pragma once
+#ifndef HEIGHTMAP_H
+#define HEIGHTMAP_H
+
+#include <DirectXMath.h>
+using namespace DirectX;
+
+class HightMap {
+
+private:
+	struct HeightMapInfo {
+		int terrainWidth;
+		int terrainHeight;
+		XMFLOAT3 *heightMap;
+	};
+
+public:
+	bool HeightMapLoad(char*, HeightMapInfo&);
+
+};
+
+
+#endif
